1164cddaedd7f9408e53b0154a444f7418849bf9
Bell App Lab <apps@bellapplab.com>
2018-07-12 14:45:49 +0200

    Submodule

--- Headers/Weakable.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for Weakable_iOS.
-FOUNDATION_EXPORT double WeakableVersionNumber;
-
-//! Project version string for Weakable_iOS.
-FOUNDATION_EXPORT const unsigned char WeakableVersionString[];
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import Weakable
-
-XCTMain([
-    testCase(WeakableTests.allTests)
-])
